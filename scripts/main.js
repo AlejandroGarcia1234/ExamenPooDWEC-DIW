9b8445f3f9b6e9a81849cb385ea831ba3a9b627f
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2023-12-04 08:37:05 +0100

    Creamos la estructura del proyecto

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const resultadoContainer = document.createElement('div');
+document.body.appendChild(resultadoContainer);
+
+export function mostrarResultado(mensaje){
+    resultadoContainer.innerHTML += `<p>${mensaje}</p>`
+}
