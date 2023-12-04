ab7cb59e275fb25dbe14cd12f7266b89177331d7
AlejandroGarcia1234 <alejandrogarciaalvarez@alumnos.tuniversformacion.es>
2023-12-04 09:21:17 +0100

    Terminamos la clase Vehiculo

---
+++ scripts/vehiculo.js
@@ -0,0 +1,82 @@
+import { mostrarResultado } from "./main"
+
+export class Vehiculo{
+    #marca
+    #modelo
+    #color
+    #fabricacion
+    #cilindrada
+
+    constructor(marca, modelo, color, fabricacion, cilindrada){
+        this.#marca = marca,
+        this.#modelo = modelo,
+        this.#color = color,
+        this.#fabricacion = fabricacion,
+        this.#cilindrada = cilindrada
+    }
+
+    mostrarDatos(){
+        const datos = `La marca del vehículo es: ${this.#marca}
+        <br /> El modelo del vehículo es: ${this.#modelo}
+        <br /> El color del vehículo es: ${this.#color}
+        <br /> El año de fabricación del vehículo es: ${this.#fabricacion}
+        <br /> La cilindrada del vehículo es: ${this.#cilindrada}
+        `
+
+        mostrarResultado(`<p>${datos}</p>`)
+    }
+
+    acelerar(velocidad){
+        const mensaje = `Se ha alcanzado una velocidad de: ${velocidad}`
+        mostrarResultado(`<p>${mensaje}</p>`)
+    }
+
+    arrancar(){
+        const mensaje = `El vehículo ${this.#marca} ha arrancado`
+        mostrarResultado(`<p>${mensaje}</p>`)
+    }
+
+    frenar(){
+        const mensaje = `El vehículo ${this.#marca} ha frenado`
+    }
+
+    get marca(){
+        return this.#marca;
+    }
+
+    set marca(value){
+        this.#marca = value;
+    }
+
+    get modelo(){
+        return this.#modelo;
+    }
+
+    set modelo(modelo){
+        this.#modelo = modelo;
+    }
+
+    get color(){
+        return this.#color;
+    }
+
+    set color(color){
+        this.#color = color;
+    }
+
+    get fabricacion(){
+        return this.#fabricacion;
+    }
+
+    set fabricacion(fabricacion){
+        this.#fabricacion = fabricacion;
+    }
+
+    get cilindrada(){
+        return this.#cilindrada;
+    }
+
+    set cilindrada(cilindrada){
+        this.#cilindrada = cilindrada;
+    }
+}
